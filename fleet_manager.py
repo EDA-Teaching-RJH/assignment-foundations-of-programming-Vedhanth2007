95f6c1e39291d287edf68cf6c643b27632a5d74f
Vedhanth2007 <vs396@kent.ac.uk>
2026-02-04 12:04:19 +0000

    added init_database

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names = ["James Kirk","Jean-Luc Picard","Spock","Kathryn Janeway","Montgomery Scott"]
+    divs = ["Command","Command","Science","Command","Engineering"]
+    ranks = ["Captain","Captain","Commander","Captain","Lt.Commander"]
+    ids = ["176","215","276","304","154"]
+    return names,divs,ranks,ids
